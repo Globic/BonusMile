d1f3680aebb6ffdc2ca5ecbddb7a71f20e15b9a7
Sergey Spevak <globic813@gmail.com>
2022-01-05 13:26:57 +0300

    .gitignore added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 85;
+        int mile = price / 20;
+        System.out.println("Вам начислят" + mile + "миль");
+    }
+}
